550214ad3f1763e4149da0a84e21a67d55e58d54
roselle nicolas <roselle.nicolas.pro@gmail.com>
2021-03-12 16:37:46 +0100

    inti webpack (voir readme): compressor-front

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# compressor-front
-partie front de l'API compressor
+# empty-project
+
+Empty project.
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+```sh
+npm start
+```
+
+## Sass
+```sass
+automatic run
+```
+"node-sass": "^5.0.0",
+"sass-loader": "^11.0.1"
+## babel
+"@babel/core": "^7.13.10",
+"@babel/preset-env": "^7.13.10",
+"babel-loader": "^8.2.2",
+"babel-polyfill": "^6.26.0"
+
+## reset css
+https://meyerweb.com/eric/tools/css/reset/
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: {
+    polyfill: "babel-polyfill",
+    app: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+};
+
+module.exports = config;
