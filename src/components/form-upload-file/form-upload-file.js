fbaced4d8b3eb55b5ba9f11be120cfbd206b0df4
DevonTheFloor <devonthefloor@free.fr>
2021-03-28 20:17:57 +0200

    input range

--- src/components/form-upload-file/form-upload-file.js
+++ src/components/form-upload-file/form-upload-file.js
@@ -1,16 +1,43 @@
 import './form-upload-file.scss'
+// import './form-upload-file-service'
 export default class FormUploadForm extends HTMLElement{
     constructor() {
         super()
         this.innerHTML = `
-        <form class="form-upload-file card">
-        <label>
-        Selectionez votre fichier
-        <input type="file" class="input-file">
-        </label>
-        <button type="submit" disabled>Envoyer</button>
-        </form>
-        `
-        import ('./form-upload-file-service')
+            <form class="form-upload-file card">
+            <label>
+            Selectionez votre fichier
+            <input type="file" class="input-file">
+            </label>
+            <label>Taux de compression :
+            <input type="range" id="tcomp" min="10" max="100" value="50" step="5" list="tocomp">
+            <datalist id="tocomp">
+                <option value="10" label="10%">
+                <option value="15">
+                <option value="20">
+                <option value="25">
+                <option value="30">
+                <option value="35">
+                <option value="40">
+                <option value="45">
+                <option value="20">
+                <option value="50" label="50%">
+                <option value="55">
+                <option value="60">
+                <option value="65">
+                <option value="70">
+                <option value="75">
+                <option value="80">
+                <option value="85">
+                <option value="90">
+                <option value="95">
+                <option value="100" label="100%">
+            </datalist>
+
+            </label>
+            <button type="submit" disabled>Envoyer</button>
+            </form>
+            `
+            import ('./form-upload-file-service')
     }
 }
--- src/pages/accueil/acceuil-page-service.js
+++ src/pages/accueil/acceuil-page-service.js
@@ -5,17 +5,26 @@ const sendImageFile = (event) => {
     event.preventDefault()
     store.dispatch(getTitle())
     const image = event.currentTarget[0].files[0]
+    const num_tcomp = document.getElementById('tcomp').value
+    const tcomp = String (100 - num_tcomp)
     // const test = JSON.stringify({
     //     test: 'test ok !'
     // })
     const formData = new FormData()
     // formData.append('test', test)
+    
     formData.append('image', image)
+    formData.append('tcomp', tcomp)
+    console.log('get image :')
     console.log(formData.get('image'))
-    fetch("http://localhost:3333/api/test", {
+    console.log('get t-comp :')
+    console.log(formData.get('tcomp'))
+    console.log('formData :', formData)
+    fetch("http://localhost:3333/api/test/choosecomp/", {
         method: "POST",
         body: formData
     })
+    .then(res => console.log('lien :', res))
 }
 
 export {sendImageFile}
