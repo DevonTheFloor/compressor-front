56905be12a117bfcd356f98e56d5b07b2a9a7d2a
roselle nicolas <roselle.nicolas.pro@gmail.com>
2021-04-07 13:56:15 +0200

    ajout d'un alert de validation de form-upload-file

--- src/components/form-upload-file/form-upload-file-service.js
+++ src/components/form-upload-file/form-upload-file-service.js
@@ -1,4 +1,5 @@
 import $dom from '../../helpers/dom'
+import Swal from 'sweetalert2'
 
     const inputFile = $dom.elm('#input-single-file')
     inputFile.addEventListener('change', (e) => {
@@ -20,6 +21,14 @@ import $dom from '../../helpers/dom'
             //active le bouton d'envoie de fichier
             btnSubmit.disabled = false
         }else {
+            //alert l'utilisateur que son fichier n'est pas valide
+            Swal.fire(
+                {
+                    icon: 'error',
+                    title: "Oops...",
+                    text: "Ce fichier n'est pas valide. Seul les images (pgn, jpeg, jpg, svp et gif) sont accepté ! Selectionner une autre image, merci.",
+                }
+              )
             //montre à l'utilisateur que le fichier est invalide
             formUploadFileLabel.classList.remove("input-valid");
             fileReturn.style.color = "red"
