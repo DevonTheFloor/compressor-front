2c05454f369de8b1b858b87b1591d2f495e7ebd3
nicolas roselle <60002258+roselle-nicolas@users.noreply.github.com>
2021-05-07 14:28:35 +0200

    revue de code

--- src/components/header-bar/header-bar.js
+++ src/components/header-bar/header-bar.js
@@ -1,34 +1,34 @@
-import "./header-bar.scss"
-import $dom from '../../helpers/dom'
-import store from '../../redux/rooterducer'
+import "./header-bar.scss";
+// import $dom from "../../helpers/dom";
+// import store from "../../redux/rooterducer";
 
 export default class HeaderBar extends HTMLElement {
 
     // static get observedAttributes() { return ['title']; }
 
     constructor(_title = "Compressor") {
-        super()
-        this._title = _title
+        super();
+        this._title = _title;
     }
 
     connectedCallback() { 
-        if (this.getAttribute('_title')) {
-            this._title = this.getAttribute('_title')
-                console.log(this.getAttribute('_title'), "attribue");
+        if (this.getAttribute("_title")) {
+            this._title = this.getAttribute("_title");
+            console.log(this.getAttribute("_title"), "attribue");
         }   
         console.log(this._title, "variable");
-            this.innerHTML = `
-                <header class="header-bar">
-                    <h1>${this._title}</h1>
-                </header>
-            `
+        this.innerHTML = `
+            <header class="header-bar">
+                <h1>${this._title}</h1>
+            </header>
+        `;
         
     }
 
     attributeChangedCallback(nameAtr, oldValue, newValue) {
         if (nameAtr === "_title" && newValue !== oldValue) {
-            this._title = newValue
-            this.querySelector('header h1').innerHTML = this._title
+            this._title = newValue;
+            this.querySelector("header h1").innerHTML = this._title;
         }
     }
 
